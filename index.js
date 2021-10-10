0ae399d23acd8f133894fc4d5ea5663015a26828
HirenGamitDcs22 <91384225+HirenGamitDcs22@users.noreply.github.com>
2021-10-10 09:59:58 +0000

    added and updated

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const rs=require("readline-sync");
 const jsondata=require("./about-Gujarat.json");
+const scorelist=require("./scoreList.json");
 const chalk=require("chalk");
+const fs=require("fs");
 
 var PlayerName="";
 var score=0;
@@ -25,8 +27,8 @@ var checkAns=(ans,correctAnswer)=>{
     }
 }
 
-var askQuestion=(q) => {
-    log(yellow(q.question));
+var askQuestion=(q,i) => {
+    log(yellow(1+parseInt(i),q.question));
     log(yellow(q.optionA));
     log(yellow(q.optionB));
     log(yellow(q.optionC));
@@ -41,10 +43,11 @@ var startGame = ()=>{
     log(green(`Hello ${PlayerName}!`));
     log("\nThere will be 10 questions.\nAnswer them with", green("a (for Option A) or b (for Option B) or c (for Option C) or d (for Option D)"), "\n")
     log(cyan("+5 points for correct answer. -2 for wrong answer.\n"));
+    scoreList=scorelist.socreBoard;
     questionlist=jsondata.questionList;
     const randomquestion=questionlist.sort(()=>.5-Math.random()).slice(0,10);
     for(i in randomquestion) {
-        const isCorrect=askQuestion(randomquestion[i]);
+        const isCorrect=askQuestion(randomquestion[i],i);
         if(isCorrect){
             log(`Your answer is ${green("Correct")}, current score is ${green(score)}\n`);
         }else{
@@ -52,12 +55,60 @@ var startGame = ()=>{
             log(blueBright("The Correct Answer is :"));
             log(green(randomquestion[i].Answer+"\n"));
         }
-    }
+    } 
     log(`Final score: ${green(score)} / 50`)
     log(green("Thank you for playing!"))
+    comparScore();
 }
 var begin=()=>{
     log(green("Welcome to the game : "),yellow("How Well Do You Know Gujarat?"));
     startGame();
 }
 begin();
+
+function comparScore(){
+  const flag=scoreList.filter(s=>parseInt(s.score)<=parseInt(score));
+  log("flag: " + flag);
+  if(flag.length>=0){
+    if(scoreList.length===5){
+        scoreList.sort(function(a,b){
+            return b.score-a.score;
+        });
+        scoreList.pop();
+    }
+    const newScore={"Name":PlayerName,"score":score}
+    fs.readFile('scoreList.json',function(err,content){
+        if(err) throw err;
+        var parseJson = JSON.parse(content);
+        parseJson.socreBoard.push(newScore);
+        fs.writeFile('scoreList.json',JSON.stringify(parseJson),function(err){
+          if(err) throw err;
+          log("hello world");
+        })
+      })
+    log(green("Congratulations! You have a new highscore."));
+    displayScoreBoard()
+    //jsondata.scoreBoard=scoreList;
+
+    // Update new scoreboard to jsonbase
+   /*  store.write('scoreboard',scoreboard).then( () => {
+      log("\nScoreboard updated")
+      
+      displayScoreBoard();
+    }) */
+  }else {
+    log(red("\nYou couldn't beat the highscore. Better luck next time!"))
+
+    displayScoreBoard()
+  }
+}
+function displayScoreBoard(){
+  scoreList.sort(function(a, b) {
+    return b.score - a.score;
+  })
+  log(yellow("\nCurrent Scoreboard:"))
+  
+  for(player of scoreList){
+    log(`Name: ${green(player.Name)}. Score: ${green(player.score)}`)
+  }
+}
